401790bf41de4ffe7002aed1716b6d8e9ce6ecd3
Tomás Zambujo <a89797@ualg.pt>
2025-11-04 16:36:28 +0000

    desisto

--- setup_db.py
+++ setup_db.py
@@ -1,4 +1,22 @@
 import sqlite3
+
 conn = sqlite3.connect("RentACar.sqlite")
+cursor = conn.cursor()
+
+# Apaga a tabela antiga se existir
+cursor.execute("DROP TABLE IF EXISTS users")
+
+# Cria a nova tabela users (só com username e password)
+cursor.execute("""
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+)
+""")
+
+conn.commit()
 conn.close()
+
+print("TA FETO!")
 
