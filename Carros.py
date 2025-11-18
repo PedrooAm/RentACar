925265b765ec14406f52b7852a8f691fb736dd86
Pedro Amaro <a89791@ualg.pt>
2025-11-18 16:57:58 +0000

    Carros.py com lista de carros

--- Carros.py
+++ Carros.py
@@ -1,5 +1,25 @@
+import sqlite3
+
+
 class Carros:
-    def executar(self):
-        print("testeeeee")
+
+    
+    def conectar(self):
+        return sqlite3.connect("Bd/RentACar.db")
+
+
+
+    def Listar(self):
+        conn = self.conectar()
+        cursor = conn.cursor()
+        cursor.execute("SELECT  modelo, marca,matricula FROM carros ")
+    
+        carros = cursor.fetchall()        
+        print("Carros Disponíveis para Aluguel:")
+        for carro in carros:
+            id, modelo, marca, ano, disponivel = carro
+            print(f"ID: {id}, Modelo: {modelo}, Marca: {marca}, Ano: {ano}")
+
+        
         
     
